f0ec57822559fc405ae3767771b722c4ecc1306c
Brian Sward <bsward@gmail.com>
2022-07-15 15:33:27 -0700

    prob5

--- js/app.js
+++ js/app.js
@@ -102,14 +102,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 
-// function multiplyArray(multArr) {
-//   let kidneyBean = multiply(((multiply(testArray[0],testArray[1])[0])),testArray[2])[0];
-//   let whiteBean = `The numbers ${testArray[0]},${testArray[1]},${testArray[2]} have a product of ${kidneyBean}.`;
-//   return [kidneyBean, whiteBean];
-// }
+function multiplyArray(multArr) {
+  let kidneyBean = multiply(((multiply(testArray[0],testArray[1])[0])),testArray[2])[0];
+  let whiteBean = `The numbers ${testArray[0]},${testArray[1]},${testArray[2]} have a product of ${kidneyBean}.`;
+  return [kidneyBean, whiteBean];
+}
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
