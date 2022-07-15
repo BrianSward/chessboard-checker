f1c2533151d8b503c32c4779a9056c7a045dea15
Brian Sward <bsward@gmail.com>
2022-07-15 15:31:51 -0700

    prob4

--- js/app.js
+++ js/app.js
@@ -75,17 +75,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-// let testArray = [2, 3, 4];
 
-// function sumArray(sumArr) {
-//   let blackBean = sum(((sum(testArray[0],testArray[1])[0])),testArray[2])[0];
-//   let refriedBean = `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${blackBean} is their sum.`;
-//   return [blackBean, refriedBean];
-// }
+let testArray = [2, 3, 4];
+
+function sumArray(sumArr) {
+  let blackBean = sum(((sum(testArray[0],testArray[1])[0])),testArray[2])[0];
+  let refriedBean = `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${blackBean} is their sum.`;
+  return [blackBean, refriedBean];
+}
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
