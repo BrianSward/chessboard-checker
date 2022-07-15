acb388f54a97ac74a68dd7ee39f79b67128c7a29
Brian Sward <bsward@gmail.com>
2022-07-15 15:29:20 -0700

    prob3

--- js/app.js
+++ js/app.js
@@ -51,16 +51,16 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function sumAndMultiply(a, b, c) { 
-//   let threeSum = sum((sum(a,b)[0]),c)[0]; //best variable name yet!
-//   let threeProd = multiply((multiply(a,b)[0]),c)[0];
-//   let threeSumString = `${a} and ${b} and ${c} sum to ${threeSum}.`;
-//   let threeProdString = `The product of ${a} and ${b} and ${c} is ${threeProd}.`;
-//   return [threeSum, threeProd, threeSumString, threeProdString];
-// }
+function sumAndMultiply(a, b, c) { 
+  let threeSum = sum((sum(a,b)[0]),c)[0]; //best variable name yet!
+  let threeProd = multiply((multiply(a,b)[0]),c)[0];
+  let threeSumString = `${a} and ${b} and ${c} sum to ${threeSum}.`;
+  let threeProdString = `The product of ${a} and ${b} and ${c} is ${threeProd}.`;
+  return [threeSum, threeProd, threeSumString, threeProdString];
+}
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
